30168603b3e797f2a00b43d17597ee6d19815214
Justin Erdmier <48148443+JustinErdmier@users.noreply.github.com>
2024-05-26 14:05:43 -0400

    feat(🐛): fix bug

--- Domain/Common/Models/AggregateRoot.cs
+++ Domain/Common/Models/AggregateRoot.cs
@@ -5,7 +5,9 @@ namespace InheritanceIssueDemo.Domain.Common.Models;
 public abstract class AggregateRoot<TId, TIdType> : Entity<TId>
     where TId : AggregateRootId<TIdType>
 {
-    protected AggregateRoot(TId id) => Id = id;
+    protected AggregateRoot(TId id)
+        : base(id) =>
+        Id = id;
 
 #pragma warning disable CS8618
     protected AggregateRoot()
